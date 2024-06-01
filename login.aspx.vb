3afbe460bb7432c29f1d0fa72d9cce4ce1864cbe
Daniel Pereira <129226312+danielp2904@users.noreply.github.com>
2024-06-01 15:45:47 -0300

    Update login.aspx.vb

--- login.aspx.vb
+++ login.aspx.vb
@@ -1,42 +1,33 @@
 Imports System.Data.SqlClient
-Imports System.Data
-Imports System.Web
 Partial Class login
     Inherits System.Web.UI.Page
 
 
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-        'pega a conexão da web confg
         Dim ConnString As String = ConfigurationManager.ConnectionStrings("form").ConnectionString
-
-        'está criando uma conexao
         Using conexao As New SqlConnection(ConnString)
             conexao.Open()
+            Dim adapter As New SqlDataAdapter
 
-            Dim query As String = "SELECT COUNT(1) from Usuario WHERE email=@email AND senha=@senha"
+            adapter.SelectCommand = New SqlCommand("SELECT top(1) COUNT(*) from Usuario WHERE Email=@email AND Senha=@senha", conexao)
+            adapter.SelectCommand.Parameters.AddWithValue("@email", txtEmail.Text)
+            adapter.SelectCommand.Parameters.AddWithValue("@senha", txtSenha.Text)
 
-            Using comando As New SqlCommand(query, conexao)
-                comando.Parameters.AddWithValue("@email", txtEmail.Text)
-                comando.Parameters.AddWithValue("@senha", txtSenha.Text)
+            Dim count As Integer = CInt(adapter.SelectCommand.ExecuteScalar())
 
-                Dim count As Integer = Convert.ToInt32(comando.ExecuteScalar())
+            If count = 1 Then
+                adapter.SelectCommand = New SqlCommand("SELECT IDUsuario FROM Usuario WHERE Email=@email", conexao)
+                adapter.SelectCommand.Parameters.AddWithValue("@email", txtEmail.Text)
+                Dim ID As Integer = CInt(adapter.SelectCommand.ExecuteScalar())
+                Session("UserID") = ID
 
-                If count = 1 Then
-                    Dim sql As String = "SELECT IDUsuario FROM Usuario WHERE email=@email"
-                    comando.CommandText = sql
-                    Dim ID As Integer = Convert.ToInt32(comando.ExecuteScalar())
-                    Session("UserID") = ID
+                Session("email") = txtEmail.Text
 
-                    Session("email") = txtEmail.Text
+                Response.Redirect("default.aspx")
+            Else
+                lblMensagem.Text = "<br>Email ou senha incorretos!"
+            End If
 
-                    Response.Redirect("default.aspx")
-                Else
-                    lblMensagem.Text = "<br>Email ou senha incorretos!"
-                End If
-
-
-            End Using
 
         End Using
 
