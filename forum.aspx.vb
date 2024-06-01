b611cb66676356aed693ed0431bfa180f6007fe4
Daniel Pereira <129226312+danielp2904@users.noreply.github.com>
2024-06-01 15:46:35 -0300

    Update forum.aspx.vb

--- forum.aspx.vb
+++ forum.aspx.vb
@@ -1,74 +1,55 @@
-﻿Imports System.Net.Mail
+Imports System.Net.Mail
 Imports System.Data.SqlClient
+Imports SendEmail
 
 Partial Class forum
     Inherits System.Web.UI.Page
+    Dim EnviarEmail As New SendEmail
+
     Protected Sub btnEnviar_Click(sender As Object, e As EventArgs) Handles btnEnviar.Click
         Dim ConnString As String = ConfigurationManager.ConnectionStrings("form").ConnectionString
-
-        Dim objEnvio As New SmtpClient
-        Dim objEmail As New MailMessage()
-
-        objEnvio.Port = 587
-        objEnvio.Host = "smtp.armazemdc.inf.br"
-        objEnvio.Credentials = New Net.NetworkCredential("daniel@inovea.io", "D@N13l29")
-
-        Dim assunto As String = Request.Form("txtAssunto")
-        Dim mensagem As String = Request.Form("txtMensagem")
-
-        Dim ID As Integer
+        Dim ID As Integer = 0
 
         If Not IsNothing(Session("UserID")) Then
             ID = CInt(Session("UserID"))
 
             Using conexao As New SqlConnection(ConnString)
                 conexao.Open()
-
-                Dim sql As String = "SELECT nome, email, instituicao, campus, cidade, curso FROM contato WHERE idCadastro = @userID"
-
-                Dim query As String = "INSERT INTO forum (assunto, mensagem) VALUES (@assunto, @mensagem)"
-
+                Dim adapter As New SqlDataAdapter
                 Try
-                    Using comando As New SqlCommand(query, conexao)
-                        comando.Parameters.AddWithValue("@assunto", assunto)
-                        comando.Parameters.AddWithValue("@mensagem", mensagem)
+                    adapter.InsertCommand = New SqlCommand("INSERT INTO Forum (Assunto, Mensagem) VALUES (@assunto, @mensagem)", conexao)
+                    adapter.InsertCommand.Parameters.AddWithValue("@assunto", txtAssunto.Text)
+                    adapter.InsertCommand.Parameters.AddWithValue("@mensagem", txtMensagem.Text)
+                    adapter.InsertCommand.ExecuteNonQuery()
 
-                        comando.ExecuteNonQuery()
+                    adapter.SelectCommand = New SqlCommand("SELECT nome, email, instituicao, campus, cidade, curso FROM Usuario WHERE IDUsuario = @userID", conexao)
+                    adapter.SelectCommand.Parameters.AddWithValue("@userID", ID)
 
-                        lblMensagem.Text = "Fórum cadastrado com sucesso!"
-                    End Using
+                    Dim reader As SqlDataReader = adapter.SelectCommand.ExecuteReader()
 
-                    Using comando As New SqlCommand(sql, conexao)
-                        comando.Parameters.AddWithValue("@userID", ID)
+                    If reader.Read() Then
+                        Dim nome As String = reader("nome").ToString
+                        Dim email As String = reader("email").ToString
+                        Dim instituicao As String = reader("instituicao").ToString
+                        Dim campus As String = reader("campus").ToString
+                        Dim cidade As String = reader("cidade").ToString
+                        Dim curso As String = reader("curso").ToString
 
-                        Dim reader As SqlDataReader = comando.ExecuteReader()
+                        Dim AssuntoEmail As String = txtAssunto.Text
+                        Dim MensagemEmail As String = String.Format("Nome: {0} <br /> Email: {1} <br /> Instituição: {2} <br /> Campus: {3} <br /> Cidade: {4} <br /> Curso: {5} <br /> Mensagem: {6}", nome, email, instituicao, campus, cidade, curso, txtMensagem.Text)
 
-                        If reader.Read() Then
-                            Dim nome As String = reader("nome").ToString
-                            Dim email As String = reader("email").ToString
-                            Dim instituicao As String = reader("instituicao").ToString
-                            Dim campus As String = reader("campus").ToString
-                            Dim cidade As String = reader("cidade").ToString
-                            Dim curso As String = reader("curso").ToString
+                        EnviarEmail.SendEmail(AssuntoEmail, MensagemEmail)
 
-                            objEmail.From = New MailAddress("daniel@inovea.io")
-                            objEmail.Subject = txtAssunto.Text
-                            objEmail.Body = "Nome: " & nome & "<br />" & "Email: " & email & "<br />" & "Instituição: " & instituicao & "<br />" & "Campus: " & campus & "<br />" & "Cidade: " & cidade & "<br />" & "Curso: " & curso & "<br />" & "Mensagem: " & txtMensagem.Text
-                            objEmail.To.Add("daniel@inovea.io")
+                    End If
 
-                            objEmail.IsBodyHtml = True
-
-                            objEnvio.Send(objEmail)
-                        End If
-
-                    End Using
+                    lblMensagem.Text = "Fórum cadastrado com sucesso!"
 
                 Catch ex As Exception
                     lblMensagem.Text = "Fórum não cadastrado!"
                 End Try
             End Using
         Else
-            lblMensagem.Text = "Você precisa estar logado para postar no fórum."
+            lblMensagem.Text = "Você precisa estar logado</br>postar no fórum."
         End If
     End Sub
 
